c43697a81bc0e7552ed25182cc788fa3af8cba97
maycon <maycon.pereira.ext@amaris.com>
2021-01-20 02:48:38 -0300

    alteramdp end point

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from "axios" ;
 
-const api = axios.create({ baseURL:'http://localhost:8080' });
+/* const api = axios.create({ baseURL:'http://localhost:8080' }); */
+const api = axios.create({ baseURL:'https://frozen-escarpment-47429.herokuapp.com' });
 
 export default api ;
