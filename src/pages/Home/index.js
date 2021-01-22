7d051c693abe1d5c023c371ad74055a1ea31741d
maycon <maycon.pereira.ext@amaris.com>
2021-01-22 10:49:51 -0300

    Link para cadastro de colaboladores

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,6 @@
 import React , { Component } from 'react';
 import './style.css';
+import { Link } from 'react-router-dom' ;
 
 class Home extends Component {
 
@@ -15,6 +16,11 @@ class Home extends Component {
 
                         <div className="item-home">
                           Bem vindo ao helpeme.com.br !
+
+                            <div className="item-home">
+                                <Link className="link" to="/cadastrar-colaborador">Cadastre-se</Link>
+                            </div>
+
                         </div>
 
                         <div className="item-home">
