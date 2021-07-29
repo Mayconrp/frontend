44fe348f799eb9938ef84afda4fb92cfcc115bff
Maycon <maycon@MacBook-Air-de-Maycon.local>
2021-07-29 10:49:49 -0300

    alterando endpoint

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: 'https://frozen-escarpment-47429.herokuapp.com'
+    baseURL: 'https://infinite-atoll-37268.herokuapp.com/'
 })
 
 export const busca = async(url, setDado) => {
